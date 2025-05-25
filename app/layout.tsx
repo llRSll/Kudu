e372bd9275d9171b51524f0453f76a3bfc34fdc5
NayabKhanvict <nayabkhanvict@gmail.com>
2025-05-25 03:31:53 -0400

    add toast into add user form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
-import { metadata } from './metadata'
-import RootLayoutClient from './layout.client'
-
-export { metadata }
+import { metadata } from "./metadata";
+import RootLayoutClient from "./layout.client";
+export { metadata };
+import { ToasterProvider } from "@/components/ToasterProvider";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return <RootLayoutClient>{children}</RootLayoutClient>
-} 
+  return (
+    <RootLayoutClient>
+      {children} <ToasterProvider />
+    </RootLayoutClient>
+  );
+}
